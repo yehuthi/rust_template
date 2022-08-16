5e95fea2c6f8ef673068970f85e8b09e50506df2
Yehuthith <yehuthith@gmail.com>
2022-08-16 05:53:32 +0300

    🎉 Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# {{project-name}} [<img src="https://img.shields.io/crates/v/{{crate_name}}" align="right" />](https://crates.io/crates/{{crate_name}) [<img src="https://img.shields.io/docsrs/{{crate_name}}" align="right" />](https://docs.rs/{{crate_name}})
+
+{{description}}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+#![warn(
+	missing_docs,
+	clippy::cargo,
+	clippy::pedantic,
+	clippy::nursery,
+	clippy::std_instead_of_core,
+	clippy::alloc_instead_of_core,
+	clippy::format_push_string,
+	clippy::undocumented_unsafe_blocks,
+	clippy::string_to_string
+)]
